8004fcf50f696793c1b37d4677d68ab56625c5a0
Sam Shelton <sam@shelt.ca>
2016-06-03 01:02:52 -0500

    Improve logging

--- logger.py
+++ logger.py
@@ -1,21 +1,14 @@
 #!/usr/bin/env python
 
-def info(body):
-    msg("INFO", body)
+def info(*args):
+    msg("INFO", args)
 
-def verbose(body):
-    msg("VERB", body)
+def verbose(*args):
+    msg("VERB", args)
 
-def error(body):
-    msg("ERR ", body)
+def error(*args):
+    msg("ERR ", args)
 
-def msg(prefix, body):
-    if type(body) == list:
-        lines = body
-    elif "\n" in body:
-        lines = body.split("\n")
-    else:
-        lines = [body]
-
-    for line in lines:
-        print(prefix + "> " + line)
+def msg(prefix, args):
+    for arg in args:
+        print(prefix + "> " + str(arg))
--- util/__init__.py
+++ util/__init__.py
@@ -1,5 +1,29 @@
-import os
+import os,sys
 
 # Divide l into groups of g
 def divide(l, g):
     return [l[i:i+g] for i in range(0, len(l), g)]
+
+    
+def query(question, default="yes"):
+    valid = {"yes": True, "y": True, "ye": True,
+             "no": False, "n": False}
+    if default is None:
+        prompt = " [y/n] "
+    elif default == "yes":
+        prompt = " [Y/n] "
+    elif default == "no":
+        prompt = " [y/N] "
+    else:
+        raise ValueError("invalid default answer: '%s'" % default)
+
+    while True:
+        sys.stdout.write(question + prompt)
+        choice = input().lower()
+        if default is not None and choice == '':
+            return valid[default]
+        elif choice in valid:
+            return valid[choice]
+        else:
+            sys.stdout.write("Please respond with 'yes' or 'no' "
+                             "(or 'y' or 'n').\n")
