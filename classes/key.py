1a6526b0179e2e214bc0fe2cbb07a8f2ce4f2cf2
Sam Shelton <sam@shelt.ca>
2016-06-03 01:02:18 -0500

    Move compute_address method to a function

--- classes/key.py
+++ classes/key.py
@@ -4,6 +4,7 @@ import ecdsa as ec
 import hashlib as hl
 
 import freecoin as fc
+from freecoin.keys import *
 from freecoin.util.hashlib import b58_encode
 
 class Key:
@@ -13,7 +14,7 @@ class Key:
         else:
             self.secret = ec.SigningKey.generate(curve=ec.SECP256k1)
         self.public = self.secret.get_verifying_key()
-        self.addr = self._compute_address()
+        self.addr = compute_address(public.to_string())
     
     def to_file(self):
         os.makedirs(fc.DIR_KEYS, exist_ok=True)
@@ -24,12 +25,9 @@ class Key:
     def from_file(addr):
         secret = ec.SigningKey.generate()
         os.makedirs(fc.DIR_KEYS, exist_ok=True)
-        with open(os.path.join(fc.DIR_KEYS, addr)) as f:
-            secret.from_pem(f.read())
-        return Key(secret=secret)
-
-    def _compute_address(self):
-        a = hl.sha256(self.public.to_string()).digest()
-        b = hl.sha256(a).digest()
-        c = hl.sha1(b).digest()
-        return b58_encode(int.from_bytes(c + (hl.sha1(c).digest())[:4], byteorder='big'))
+        try:
+            with open(os.path.join(fc.DIR_KEYS, addr)) as f:
+                secret.from_pem(f.read())
+            return Key(secret=secret)
+        except FileNotFoundError:
+            return None
