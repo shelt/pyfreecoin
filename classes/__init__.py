8baba2e33b3a717dc7b54245317991001efa14ca
Sam Shelton <sam@shelt.ca>
2016-06-03 01:01:46 -0500

    Convert Hashable interface to Serializable, implement compute_size

--- classes/__init__.py
+++ classes/__init__.py
@@ -1,6 +1,9 @@
 
 from hashlib import sha256
 
-class Hashable:
+class Serializable:
     def compute_hash(self):
         return sha256(self.to_bytes()).digest()
+    
+    def compute_size(self):
+        return len(self.to_bytes())
